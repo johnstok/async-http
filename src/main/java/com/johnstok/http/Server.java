e1a8c49b3d23268eb45b4e256347e01aa69cc736
Keith Webster Johnston <k.w.johnston@gmail.com>
2011-11-21 00:02:47 +0000

    Added a server interface.

---
+++ src/main/java/com/johnstok/http/Server.java
@@ -0,0 +1,47 @@
+/*-----------------------------------------------------------------------------
+ * Copyright © 2011 Keith Webster Johnston.
+ * All rights reserved.
+ *
+ * This file is part of async-http.
+ *
+ * async-http is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * async-http is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with async-http. If not, see <http://www.gnu.org/licenses/>.
+ *---------------------------------------------------------------------------*/
+package com.johnstok.http;
+
+
+import java.net.InetSocketAddress;
+
+
+/**
+ * A HTTP server.
+ *
+ * @author Keith Webster Johnston.
+ */
+public interface Server {
+
+    /**
+     * Start the server listening.
+     *
+     * @param address        The address at which to listen.
+     * @param requestFactory The factory for requests.
+     */
+    void listen(InetSocketAddress address,
+                RequestFactory requestFactory);
+
+
+    /**
+     * Stop the server listening.
+     */
+    void close();
+}
